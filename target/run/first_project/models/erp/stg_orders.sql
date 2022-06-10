771cd1102fbb313bcdd1c9afbafd9b2a492c32d0
ambientelivre <ambientelivre@hack-sandbox.ambientelivre.com.br>
2022-06-10 15:42:19 -0300

    adicionado projeto aula sabado

---
+++ target/run/first_project/models/erp/stg_orders.sql
@@ -0,0 +1,13 @@
+
+
+  create  table "erp_oltp"."public"."stg_orders__dbt_tmp"
+  as (
+    with orders as (
+ select
+ numero_pedidos as order_id,
+ codigo_cliente as customer_id,
+ data_pedido as order_date
+ from erp_oltp.public.pedidos
+)
+select * from orders
+  );
